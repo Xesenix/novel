7f86b56965c1cb5649df6ee551380df4c0b6d392
Xesenix <pkapalla@xesenix.pl>
2017-11-23 22:43:33 +0100

    NOVEL-22 fix(routing) - fix app routing

--- src/app/routing/app-routing.module.ts
+++ src/app/routing/app-routing.module.ts
@@ -3,12 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 
 export const routes: Routes = [
 	{ path: '', redirectTo: 'story', pathMatch: 'full' },
-	{ path: 'story', loadChildren: 'story/story.module#StoryModule' },
-	{ path: 'characters', loadChildren: 'characters/characters.module#CharactersModule' },
-	{ path: 'storage', loadChildren: 'storage/storage.module#StorageModule' },
+	{ path: 'story', loadChildren: 'app/story/story.module#StoryModule' },
+	{ path: 'characters', loadChildren: 'app/characters/characters.module#CharactersModule' },
+	{ path: 'storage', loadChildren: 'app/storage/storage.module#StorageModule' },
 ];
 
 @NgModule({
+	imports: [RouterModule.forRoot(routes, { enableTracing: false })],
 	exports: [RouterModule],
 })
 export class AppRoutingModule {}
