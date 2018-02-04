a491c156932acf2cd20c41f05169e9c43f8e64fc
Xesenix <pkapalla@xesenix.pl>
2018-02-04 05:26:40 +0100

    NOVEL-29 feat(tools): add box for testing level-up component

--- src/app/routing/app-routing.module.ts
+++ src/app/routing/app-routing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PrimaryLayoutComponent } from 'layout/primary-layout/primary-layout.component';
+import { ToolsComponent } from 'app/tools/tools.component';
 
 export const routes: Routes = [
-	{ path: '', redirectTo: 'story', pathMatch: 'full' },
-	{ path: 'story', component: PrimaryLayoutComponent, loadChildren: 'app/story/story.module#StoryModule' },
-	{ path: 'characters', component: PrimaryLayoutComponent, loadChildren: 'app/characters/characters.module#CharactersModule' },
-	{ path: 'storage', component: PrimaryLayoutComponent, loadChildren: 'app/storage/storage.module#StorageModule' },
+	{
+		path: '',
+		component: PrimaryLayoutComponent,
+		children: [
+			{ path: '', redirectTo: 'story', pathMatch: 'full' },
+			{ path: '', outlet: 'aside-tools', component: ToolsComponent },
+			{ path: 'story', loadChildren: 'app/story/story.module#StoryModule' },
+			{ path: 'characters', loadChildren: 'app/characters/characters.module#CharactersModule' },
+			{ path: 'storage', loadChildren: 'app/storage/storage.module#StorageModule' },
+		],
+	},
 ];
 
 @NgModule({
